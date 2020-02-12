10615ae6d4b335c789fa9beeba7d65d1bbfb1a4d
James Taylor <jtaylor@cpicardgroup.com>
2020-02-12 21:18:30 +0000

    Dynamic Module Loading & Registration

---
+++ riffdog/resource.py
@@ -0,0 +1,80 @@
+import functools
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+
+
+class ResourceDirectory(object):
+    # This is the resource directory singleton. There can be only one.
+
+    class __ResourceDirectory:
+        found_resources = {}
+
+        def __init__(self):
+            self.found_resources = {}
+
+        def __str__(self):
+            return str(found_resources)
+
+        def add(self, key, target_type):
+            self.found_resources[key] = target_type
+
+        def lookup(self, key):
+            if key in self.found_resources:
+                return self.found_resources[key]
+            else:
+                return None
+
+            
+
+    instance = None
+    def __new__(cls): # __new__ always a classmethod
+        if not ResourceDirectory.instance:
+            ResourceDirectory.instance = ResourceDirectory.__ResourceDirectory()
+        return ResourceDirectory.instance
+
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
+
+    def __setattr__(self, name):
+        return setattr(self.instance, name)
+
+
+def register(resource_name):
+    print("in the register?")
+    
+    def actual_decorator(constructor):
+        logger.info("resource tagged %s - %s " % (resource_name , constructor))
+
+        @functools.wraps(constructor)
+        def wrapper(*args, **kwargs):
+            print("In the construct?")
+            print("inner type %s " % type(constructor))
+            return constructor(*args, **kwargs)
+
+        rd = ResourceDirectory()
+        rd.add(resource_name, wrapper)
+
+        return wrapper
+    return actual_decorator
+
+
+class Resource():
+
+
+    def fetch_real_resources(self, region):
+        # This may be called multiple times for each region in the scan list
+        # i.e. append
+        raise NotImplementedError()
+
+    def process_state_resource(state_resource):
+        # This function is called potentilly multiple times as each resource
+        # is discovered by the state scanner i.e. append results to local store
+        raise NotImplementedError()
+
+    def compare(self, depth):
+        # this function should be called once, take the local data and return
+        # an array of result elements.
+        raise NotImplementedError()
--- riffdog/scanner.py
+++ riffdog/scanner.py
@@ -1,36 +1,21 @@
 import logging 
 import json
 
+import os
+import sys
+import inspect
+import pkgutil
+from pathlib import Path
+from importlib import import_module
+
 import boto3
 
-from .modules.ec2_instances import _tf_process_instances, _boto_fetch_instances, _compare_instances
+#from .modules.ec2_instances import _tf_process_instances, _boto_fetch_instances, _compare_instances
 
 from .data_structures import ScanMode, StateStorage, RDConfig, ReportElement 
+from .resource import ResourceDirectory
 
 logger = logging.getLogger(__name__)
-
-
-# This is the list of things we don't support yet (at a minimum)
-_ignore_types = (
-                'aws_nat_gateway', 'aws_main_route_table_association', 'aws_internet_gateway', 
-                'aws_kms_key', 'aws_elasticsearch_domain', 'aws_iam_role', 'aws_db_subnet_group', 
-                'aws_alb_listener', 'aws_alb_target_group', 'aws_alb_target_group_attachment',
-                'aws_iam_policy_document', 'aws_elb_service_account', 'cloudflare_ip_ranges', 
-                'aws_s3_bucket_policy', 'aws_alb_listener_rule', 'aws_lb_listener_certificate',
-                'aws_route', 'aws_route_table_association', 'aws_default_network_acl', 'aws_elb_attachment',
-                'aws_ebs_volume', 'aws_iam_instance_profile', 'aws_iam_role_policy_attachment',
-                'aws_volume_attachment', 'aws_dynamodb_table', 'aws_iam_policy', 'aws_iam_policy_attachment'
-                'aws_default_route_table', 'aws_lb_listener', 'aws_lb_listener_rule', 
-                'aws_lb_target_group', 'aws_lb_target_group_attachment', 
-                'aws_customer_gateway', 'aws_vpn_connection', 'aws_vpn_gateway', 
-                'aws_iam_role_policy', 'aws_iam_service_linked_role', 
-                'aws_lb_listener_certificate', 'aws_s3_bucket_notification', 'aws_ecr_repository',
-                'aws_iam_policy_attachment', 'aws_default_route_table', 'aws_sqs_queue_policy'
-               )
-
-_scan_map = {
-    'aws_instance': { 'aws_scan_func': _boto_fetch_instances, 'compare_func': _compare_instances }
-}
 
 
 
@@ -45,26 +30,43 @@ def scan(config):
 
     # Future JT will hate past JT for these assumptions.
 
-    items = _get_blank_state_dict()
+    # Build scan map:
+    rd = ResourceDirectory()
+
+    for (_, name, _) in pkgutil.iter_modules([os.path.join(os.path.dirname(__file__),"resources")]):
+        imported_module = import_module('riffdog.resources.%s' % name)
+        # for element in dir(import_module):
+        #     if not element.startswith("__"):
+        #         suspect_class = getattr(imported_module, class_name[0])
+        #         if issubclass(suspect_class, Resource):
+        #             target = suspect_class.register()
+        #             logging.info("Found %s -> %s " % (target, element))
+        #         else:
+        #             logging.info("This %s is not a thing" %)
+
+    for r in ResourceDirectory.instance.found_resources.keys():
+        print(r)
+ 
+
+   # fish_class = getattr(imported_module, class_name[0])
+
+   # if issubclass(fish_class, AnimalBaseClass):
+   #     setattr(sys.modules[__name__], name, fish_class)
+
+   # items = _get_blank_state_dict()
+
 
     if config.state_storage == StateStorage.AWS_S3:
         # Note we don't support mix & match state locations.
         # Message me if you do this. Becuase I'm interested as to why
         for state_folder in config.state_file_locations:
             logger.info("Inspecting %s" % state_folder)
-            new_items = _s3_state_fetch(state_folder)
-            
-            for key, val in items.items():
-                logger.info("Adding %s items of type %s" % (len(new_items[key]), key))
-                val.update(new_items[key])
+            found = _s3_state_fetch(state_folder)
 
     else:
         raise NotImplemented("State storage not implemented yet")
 
     # Extract items of interest from States:
-    logger.info("Total items found:")
-    for key, val in items.items():
-        logger.info("%s:\t%s" % (key, len(val)))
 
 
     logger.info("Now Looking at AWS (via Boto)")
@@ -73,22 +75,21 @@ def scan(config):
 
     for scan_element in config.elements_to_scan:
         logger.info("Now inspecting %s" % scan_element)
-        real_items = {}
-        for region in config.regions:
-            real_items.update(_scan_map[scan_element]['aws_scan_func'](region))
-            
-        report[scan_element] = _scan_map[scan_element]['compare_func'](items[scan_element], real_items, config)
 
+        # DISCUSS: hope they're all registered! - also? is this still relevant 
+        
+        if not scan_element in found:
+            found[scan_element] = rd.lookup(scan_element)() # put a blank one in
+        
+        for region in config.regions:
+            found[scan_element].fetch_real_resources(region)
+    
+        # now compare
+        report[scan_element] =  found[scan_element].compare(config, None) # FIME - set depth
 
     logger.info("Scan Complete")
     return report
 
-def _get_blank_state_dict():
-    return {
-        "aws_instance": {},
-        "buckets": {},
-        "rds": {}
-    }
 
 def _s3_state_fetch(bucket_name):
     """
@@ -102,104 +103,47 @@ def _s3_state_fetch(bucket_name):
 
     items = client.list_objects(Bucket=bucket_name, Prefix="")
     
-    total = _get_blank_state_dict()
+    found = {}
 
     for item in items['Contents']:
-        found = None
         #print(item['Key'])
         if item['Key'].endswith("terraform.tfstate"):
-            found = search_state(bucket_name, item['Key'], s3)
-
-            for key, val in total.items():
-                val.update(found[key])
+            found = _search_state(bucket_name, item['Key'], s3, found)
 
     # instances
-    return total
+    return found
 
 
-def search_state(bucket_name, key, s3):
+def _search_state(bucket_name, key, s3, found):
     obj = s3.Object(bucket_name, key)
     content = obj.get()['Body'].read()
     parsed = json.loads(content)
-
-    outs = _get_blank_state_dict()
-
-    unknowns = []
+    from .resource import Resource
 
     try:
+        rd = ResourceDirectory()
         for res in  parsed['resources']:
             #print(res['type'])
-            if res['type'] in _ignore_types or not res['type'].startswith('aws_'):
-                # ignore known
-                pass
-
-            elif res['type'] == "aws_instance":
-                found = _tf_process_instances(res, key)
-                outs['aws_instance'].update(found)
-
-            elif res['type']=="aws_s3_bucket":
-                found = _process_s3(res, key)
-                outs['buckets'].update(found)
-            elif res['type'] == "aws_sqs_queue":
-                # FIXME: write queue scanner
-                pass
-            elif res['type'] == "aws_alb":
-                # FIXME: write Alb scanner
-                pass
-            elif res['type'] == "aws_elb":
-                # FIXME: write elb 
-                pass
-            elif res['type'] == "aws_lb":
-                # FIXME: classic lb
-                pass
-            elif res['type'] == "aws_security_group":
-                # FIXME: write security group scanner
-                pass
-            elif res['type'] == 'aws_security_group_rule':
-                # FIXME: write this?
-                pass
-            elif res['type'] == 'aws_subnet':
-                # FIXME: write subnet scanner
-                pass
-            elif res['type'] == 'aws_vpc':
-                # FIXME: write vpc scanner
-                pass
-            elif res['type'] == 'aws_route_table':
-                # FIXME: write routetable scanner
-                # might not be relevant
-                pass
-            elif res['type'] == 'aws_vpc_peering_connection':
-                # FIXME: write vpc peering conenction scanner
-                pass
-            elif res['type'] == 'aws_eip':
-                # FIXME: write eip scanner
-                pass
-            elif res['type'] == 'aws_network_interface':
-                # FIXME: nework interface
-                pass
-            elif res['type'] == 'aws_rds_cluster':
-                # RDS Cluster
-                pass
-            elif res['type'] == 'aws_rds_cluster_instance':
-                # RDS cluster instance
-                pass
+            if res['type'] in found:
+                # its already loaded!
+                logging.debug("Resource Matched Existing %s" % res['type'])
+                found[res['type']].process_state_resource(res, key)
             else:
-                # warn on unknown types
-                if res['type'] not in unknowns:
-                    unknowns.append(res['type'])
-                
-        if len(unknowns) > 0:
-            unknown_str = ""
-            for unknown in unknowns:
-                unknown_str += ", %s" % unknown
-        
-            logger.warning("The following elements were unknown " + unknown_str)
+                func = rd.lookup(res['type'])
+                if func:
+                    logging.debug("Resource Matched %s - creating" % res['type'])
+                    found[res['type']] = func()
+                    found[res['type']].process_state_resource(res, key)
+                else:
+                    logging.debug("Unsupported resource %s" % res['type'])
+
 
     except Exception as e:
+        logger.info(e)
         logger.warning("Bad Terraform file: %s (perhaps a TF/state version issue?) %s" % (key, type(e)))
         pass
 
-    return outs
+    return found 
 
 def _process_s3(s3, state_filename):
     
