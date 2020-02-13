9843b111d19416b4f4a1f0591c2edf6934607fcb
David Reynolds <david.reynolds@hubbub.net>
2020-02-13 12:43:44 +0000

    Style fixes

--- riffdog/resources/s3_buckets.py
+++ riffdog/resources/s3_buckets.py
@@ -1,5 +1,5 @@
 """
-This module is for EC2 instance processing - terraform & boto and comparison.
+This module is for S3 bucket processing - terraform & boto and comparison.
 """
 
 import logging
@@ -8,11 +8,11 @@ from ..resource import AWSResource, register
 
 logger = logging.getLogger(__name__)
 
+
 @register("aws_s3_bucket")
 class S3Buckets(AWSResource):
     _states_found = {}
     _real_buckets = {}
-
 
     def fetch_real_resources(self, region):
         logging.info("Looking for s3 resources")
@@ -24,11 +24,8 @@ class S3Buckets(AWSResource):
         for bucket in response['Buckets']:
             self._real_buckets[bucket['Name']] = bucket
 
-        
     def process_state_resource(self, state_resource, state_filename):
-        
         self._states_found[state_resource['name']] = state_resource
-
 
     def compare(self, config, depth):
         # this function should be called once, take the local data and return
@@ -52,6 +49,5 @@ class S3Buckets(AWSResource):
             if key not in self._states_found:
                 out_report.in_real_but_not_tf.append(key)
 
-        print()
         return out_report
 
