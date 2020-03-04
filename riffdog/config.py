b48eb0f465b892f7bafb22cbb4b63d23ad787ffe
David Reynolds <david@reynoldsfamily.org.uk>
2020-03-04 21:15:46 +0000

    Fix `--exclude-resource` support and add `--include-resource` support
    If anything is in `include-resource` it completely overrides
    `exclude-resource` so it's like starting from a blank list

--- riffdog/config.py
+++ riffdog/config.py
@@ -24,8 +24,11 @@ class RDConfig:
 
         @property
         def elements_to_scan(self):
-            return (x for x in self.base_elements_to_scan if x not in self.excluded_resources)
-
+            if self.included_resources:
+                resources = (x for x in self.included_resources)
+            else:
+                resources = (x for x in self.base_elements_to_scan if x not in self.excluded_resources)
+            return resources
 
         def __init__(self):
             # Set defaults and must-have settings
@@ -37,6 +40,7 @@ class RDConfig:
             self.state_file_locations = []
 
             self.excluded_resources = []
+            self.included_resources = []
 
             self.base_elements_to_scan = []
 
